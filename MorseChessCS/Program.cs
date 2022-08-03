765cc2c699482a965ec03d0c67ebec4caaafe1da
BestIsaacPlayer <milosz.plask@gmail.com>
2022-08-03 17:55:05 +0200

    adds texture loading, board creation and main game loop

---
+++ MorseChessCS/Program.cs
@@ -0,0 +1,32 @@
+﻿using Raylib_cs;
+
+namespace MorseChessCS
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<IDrawable> drawables = new();
+
+            Raylib.InitWindow(GameManager.GameWidth, GameManager.GameHeight, "MorseChessCS");
+            Raylib.SetTargetFPS(60);
+            GameManager.LoadPieceTextures();
+            Board board = new();
+
+            drawables.Add(board);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+                foreach (IDrawable drawable in drawables)
+                {
+                    drawable.Draw();
+                }
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
