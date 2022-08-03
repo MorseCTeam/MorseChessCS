1dcf4d7cb0c129a7b34d8040163058e6db9697b4
BestIsaacPlayer <milosz.plask@gmail.com>
2022-08-03 17:56:36 +0200

    adds abstract piece class with all informations needed to distinguish and render chess pieces

---
+++ MorseChessCS/Piece.cs
@@ -0,0 +1,45 @@
+﻿using Raylib_cs;
+
+namespace MorseChessCS
+{
+    abstract class Piece : IDrawable
+    {
+        protected int piecePointValue;
+        protected PieceColor pieceColor;
+        protected PieceName pieceName;
+        protected Texture2D pieceTexture;
+        protected string pieceKey;
+
+
+        public enum PieceColor
+        {
+            White,
+            Black
+        }
+
+        protected string[] stringColor = { "White", "Black" };
+
+        public enum PieceName
+        {
+            Pawn,
+            Rook,
+            Knight,
+            Bishop,
+            Queen,
+            King
+        }
+
+        protected string[] stringName = { "Pawn", "Rook", "Knight", "Bishop", "Queen", "King" };
+
+        public PieceColor getPieceColor()
+        {
+            return pieceColor;
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+
+}
