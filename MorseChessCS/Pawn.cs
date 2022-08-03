abbb43db0783ca25cb495edd6fb14f14177b677d
BestIsaacPlayer <milosz.plask@gmail.com>
2022-08-03 17:57:45 +0200

    adds a Pawn class and sets its basic constructor

---
+++ MorseChessCS/Pawn.cs
@@ -0,0 +1,15 @@
+﻿namespace MorseChessCS
+{
+    internal class Pawn : Piece
+    {
+        public Pawn(PieceColor color)
+        {
+            piecePointValue = 1;
+            pieceColor = color;
+            pieceName = PieceName.Pawn;
+            pieceKey = stringColor[(int)pieceColor] + stringName[(int)pieceName];
+            pieceTexture = GameManager.PieceTextures[pieceKey];
+        }
+
+    }
+}
