005adbf23a823a0c77522855855f9640248954ac
BestIsaacPlayer <milosz.plask@gmail.com>
2022-08-03 17:59:12 +0200

    adds GameManager class for textures and window management

---
+++ MorseChessCS/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using Raylib_cs;
+namespace MorseChessCS
+{
+    public static class GameManager
+    {
+        public static int GameWidth { get; set; } = 1024;
+        public static int GameHeight { get; set; } = 800;
+        public static int BoardLength { get => (GameWidth > GameHeight ? GameHeight : GameWidth); }
+
+        public static Dictionary<string, Texture2D> PieceTextures { get; set; } = new();
+
+        public static void LoadPieceTextures()
+        {
+            foreach (string color in Enum.GetNames(typeof(Piece.PieceColor)))
+            {
+                foreach (string name in Enum.GetNames(typeof(Piece.PieceName)))
+                {
+                    PieceTextures.Add($"{color}{name}", Raylib.LoadTexture($"./assets/{color}{name}.png"));
+                }
+            }
+
+        }
+    }
+}
