44897584c23206615e73d5e24cdc7f812f56cd24
BestIsaacPlayer <milosz.plask@gmail.com>
2022-08-06 13:57:17 +0200

    adds default Knight class

---
+++ MorseChessCS/Knight.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace MorseChessCS
+{
+    internal class Knight : Piece
+    {
+        public Knight(PieceColor color)
+        {
+            pointValue = 3;
+            Color = color;
+            Type = PieceType.Knight;
+            Texture = GameManager.PieceTextures[Color.ToString() + Type.ToString()];
+        }
+
+        public override List<Vector2> GetPossibleMoves(Vector2 mousePosition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
