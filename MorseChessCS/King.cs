5b943c382db254150499c246012aeaaa4abe64bf
BestIsaacPlayer <milosz.plask@gmail.com>
2022-08-06 13:57:02 +0200

    adds default King class

---
+++ MorseChessCS/King.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace MorseChessCS
+{
+    internal class King : Piece
+    {
+        public King(PieceColor color)
+        {
+            pointValue = 0;
+            Color = color;
+            Type = PieceType.King;
+            Texture = GameManager.PieceTextures[Color.ToString() + Type.ToString()];
+        }
+
+        public override List<Vector2> GetPossibleMoves(Vector2 mousePosition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
