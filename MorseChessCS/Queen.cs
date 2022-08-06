da8d050e6386547a45d9ceadd5cc2e89529b51fa
BestIsaacPlayer <milosz.plask@gmail.com>
2022-08-06 13:59:53 +0200

    adds default Queen class

---
+++ MorseChessCS/Queen.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace MorseChessCS
+{
+    public class Queen : Piece
+    {
+        public Queen(PieceColor color)
+        {
+            pointValue = 9;
+            Color = color;
+            Type = PieceType.Queen;
+            Texture = GameManager.PieceTextures[Color.ToString() + Type.ToString()];
+        }
+
+        public override List<Vector2> GetPossibleMoves(Vector2 mousePosition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
