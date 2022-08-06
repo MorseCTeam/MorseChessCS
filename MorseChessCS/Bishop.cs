822534008293e14f257933802b811d24a6fe9dce
BestIsaacPlayer <milosz.plask@gmail.com>
2022-08-06 13:55:28 +0200

    adds default Bishop class

---
+++ MorseChessCS/Bishop.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace MorseChessCS
+{
+    internal class Bishop : Piece
+    {
+        public Bishop(PieceColor color)
+        {
+            pointValue = 3;
+            Color = color;
+            Type = PieceType.Bishop;
+            Texture = GameManager.PieceTextures[Color.ToString() + Type.ToString()];
+        }
+
+        public override List<Vector2> GetPossibleMoves(Vector2 mousePosition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
