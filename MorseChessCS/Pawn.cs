51735aef03f630df4cb03b88c30a6492bd7630f3
BestIsaacPlayer <milosz.plask@gmail.com>
2022-08-06 13:58:25 +0200

    adds functionality for selected pawn

--- MorseChessCS/Pawn.cs
+++ MorseChessCS/Pawn.cs
@@ -1,14 +1,86 @@
-﻿namespace MorseChessCS
+﻿using System.Numerics;
+
+namespace MorseChessCS
 {
     internal class Pawn : Piece
     {
         public Pawn(PieceColor color)
         {
-            piecePointValue = 1;
-            pieceColor = color;
-            pieceName = PieceName.Pawn;
-            pieceKey = stringColor[(int)pieceColor] + stringName[(int)pieceName];
-            pieceTexture = GameManager.PieceTextures[pieceKey];
+            pointValue = 1;
+            Color = color;
+            Type = PieceType.Pawn;
+            Texture = GameManager.PieceTextures[Color.ToString() + Type.ToString()];
+        }
+
+        public override List<Vector2> GetPossibleMoves(Vector2 mousePosition)
+        {
+            int fieldLength = GameManager.FieldLength;
+            List<Vector2> possibleMoves = new();
+
+            int x = (int)mousePosition.X / fieldLength;
+            int y = (int)mousePosition.Y / fieldLength;
+            int count = (Color == PieceColor.White && y == 6) || (Color == PieceColor.Black && y == 1) ? 2 : 1;
+            int correct = count == 2 ? 3 : 2;
+            Board board = GameManager.Board;
+            switch (Color)
+            {
+                case PieceColor.White:
+                {
+                    while (count > 0)
+                    {
+                        if (board.pieces[x,y-Math.Abs(count - correct)] is not null)
+                        {
+                          break;        
+                        }
+                        possibleMoves.Add(new Vector2(x, y - Math.Abs(count - correct)));
+                        count--;
+                    }
+                    if (x != 7)
+                    {
+                        if (board.IsFieldOccupied(x + 1, y - 1) && board.pieces[x, y].Color != board.pieces[x + 1, y - 1].Color)
+                        {
+                            possibleMoves.Add(new Vector2(x + 1, y - 1));
+                        }
+                    }
+                    if (x != 0)
+                    {
+                        if (board.IsFieldOccupied(x - 1, y - 1) && board.pieces[x, y].Color != board.pieces[x - 1, y - 1].Color)
+                        {
+                            possibleMoves.Add(new Vector2(x - 1, y - 1));
+                        }
+                    }
+                    break;
+                }
+                case PieceColor.Black:
+                {
+                 
+                    while (count > 0)
+                    {
+                        if (board.pieces[x, y + Math.Abs(count - correct)] is not null)
+                        {
+                            break;
+                        }
+                        possibleMoves.Add(new Vector2(x, y + Math.Abs(count - correct)));
+                        count--;
+                    }
+                        if (x != 7)
+                        {
+                            if (board.IsFieldOccupied(x + 1, y + 1) && board.pieces[x, y].Color != board.pieces[x + 1, y + 1].Color)
+                            {
+                                possibleMoves.Add(new Vector2(x + 1, y + 1));
+                            }
+                        }
+                        if (x != 0)
+                        {
+                            if (board.IsFieldOccupied(x - 1, y + 1) && board.pieces[x, y].Color != board.pieces[x - 1, y + 1].Color)
+                            {
+                                possibleMoves.Add(new Vector2(x - 1, y + 1));
+                            }
+                        }
+                        break;
+                }
+            }
+            return possibleMoves;
         }
 
     }
